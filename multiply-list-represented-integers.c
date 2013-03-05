d324e9f3623d1aa6d1bcc87649e6c17e8c62fb25
Wang Guibao <wang_guibao@163.com>
2013-03-05 23:57:23 +0800

    correct by add after multiply

--- multiply-list-represented-integers.c
+++ multiply-list-represented-integers.c
@@ -38,24 +38,29 @@ void init_lists(NODE *m1, NODE *m2)
     p->next = NULL;
 }
 
-int multiply_to(NODE *m1, int multiplier)
+int multiply_to(NODE *m1, int multiplier, NODE *p_result)
 {
     int carrier = 0;
-    int result;
+    int product;
     NODE *p = m1;
+    NODE *p_temp;
 
     if (p->next != NULL) {
-        carrier = multiply_to(p->next, multiplier);
+        carrier = multiply_to(p->next, multiplier, p_result);
     }
-    result = p->n * multiplier + carrier;
-    p->n = result % 10;
 
-    return result / 10;
+    product = p->n * multiplier + carrier;
+    p_temp = malloc(sizeof(NODE));
+    p_temp->n = product % 10;
+    p_temp->next = p_result;
+    p_result = p_temp;
+
+    return product / 10;
 }
 
 void multiply_to_10(NODE *m1)
 {
-    NODE *p = p1;
+    NODE *p = m1;
 
     while (p->next != NULL) {
         p = p->next;
@@ -71,23 +76,24 @@ void multiply(NODE *m1, NODE *m2)
 {
     NODE *p1 = m1;
     NODE *p2 = m2;
-    NODE *p = p1;
+    NODE *p_temp = NULL;
+    NODE *p_result = NULL;
     int multiplier;
 
     while (p2 != NULL) {
         multiplier = p2->n;
         p2 = p2->next;
 
-        if (multiplier == 0) {
-        multiply_to_10(p1);
+        if (multiplier == 0 && p_temp != NULL) {
+            multiply_to_10(p_temp);
         }
         else {
-            int carrier = multiply_to(p1, multiplier);
+            int carrier = multiply_to(p1, multiplier, p_temp);
             if (carrier > 0) {
-                p = malloc(sizeof(NODE));
+                NODE *p = malloc(sizeof(NODE));
                 p->n = carrier;
-                p->next = p1;
-                p1 = p;
+                p->next = p_temp;
+                p_temp = p;
             }
         }
     }
