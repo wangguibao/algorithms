df54cb1df0363cdff109b4651cf3666b2bd24d09
Wang Guibao <wang_guibao@163.com>
2013-08-09 21:23:34 +0800

    Update comments

--- treeutil.c
+++ treeutil.c
@@ -1,3 +1,7 @@
+/*
+ * treeutil.c - tree utilities library
+ * Author: Wang Guibao <wang_guibao#163.com>
+ */
 #include <stdio.h>
 #include <string.h>
 #include "treeutil.h"
@@ -5,11 +9,33 @@
 #define MAX_LEN 32
 static char indent[(MAX_LEN + 1) * 2];
 
+/**
+ * tu_init
+ * Initialization work for treeutil library.
+ * This function should be called before other functions in this library
+ */
 void tu_init()
 {
     indent[0] = '\0';
 }
 
+/**
+ * tu_print_tree
+ *
+ * Graphically print out binary tree
+ *
+ * NOTE: 1) Call tu_init() before calling this function
+ *       2) First part of application tree nodes should have the same structure as that TREE_NODE
+ *          so that type cast won't harm you (and your device)
+ * Use:
+ *     // assuming root points to root of the binary tree and below visit function
+ *     // void tree_visit(NODE *node);
+ *     tu_print_tree(ROOT, RIGHT, visit)
+ *
+ * @param tree  root to the tree
+ * @param dir   tree traversal direction
+ * @param visit node visit function pointer
+ */
 void tu_print_tree(TREE_NODE *tree, DIRECTION dir, tu_node_visitor_t visit)
 {
     if (tree != NULL) {
@@ -32,4 +58,3 @@ void tu_print_tree(TREE_NODE *tree, DIRECTION dir, tu_node_visitor_t visit)
     }
     return;
 }
-
