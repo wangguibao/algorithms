81096b39cf389db71b71a68b691c635542d57049
Wang Guibao <wang_guibao@163.com>
2017-07-23 16:52:35 +0800

    Create sieve_of_eratosthenes.c

---
+++ sieve_of_eratosthenes.c
@@ -0,0 +1,67 @@
+/**
+ * @file sieve_of_eratosthenes.c
+ * @author Wang Guibao (https://github.com/wangguibao/)
+ * @brief Find all prime numbers within a given range, using Sieve of Eratosthenes
+ *
+ * https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+ */
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX_ELE 1024
+
+void sieve_of_eratosthenes(int n)
+{
+    int a[MAX_ELE + 1] = {0};
+    int i = 0;
+    int j = 0;
+    if (n > MAX_ELE) {
+        printf("Out of range (%u)\n", MAX_ELE);
+        return;
+    }
+
+    for (i = 0; i <= n; ++i) {
+        a[i] = i;
+    }
+
+    i = 2;
+    while (i * i <= n) {
+        for (j = i * i; j <= n; j += i) {
+            a[j] = 0;
+        }
+
+        for (j = i + 1; j * j <= n; ++j) {
+            if (a[j] != 0) {
+                i = j;
+                break;
+            }
+        }
+
+        if (j * j >= n) {
+            break;
+        }
+    }
+
+    for (i = 2; i <= n; ++i) {
+        if (a[i] != 0) {
+            printf("%u ", a[i]);
+        }
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n = 0;
+    int ret = 0;
+    printf("Integer range in which to compute all prime numbers (max %u): ", MAX_ELE);
+
+    ret = scanf("%u", &n);
+    if (ret != 1 || n <= 1 || n > MAX_ELE) {
+        printf("Invalid range %u\n", n);
+        return -1;
+    }
+
+    sieve_of_eratosthenes(n);
+    return 0;
+}
